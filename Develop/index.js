e7231073c26711ef7074064663c52a5faf09f0a3
Fabitheguy003 <fabienmukungu@gmail.com>
2023-02-09 18:08:47 -0500

    added argument and completed finction

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,25 @@
 // TODO: Include packages needed for this application
+const fs = require{"fs"};
+const requirer = require{'inquirer'};
+
 
 // TODO: Create an array of questions for user input
 const questions = [];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) => {
+    if (err) throw err;
+    console.log('README file created!');
+    });
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions).then((answers) => {
+    writeToFile("README.md", answers);
+    });
+}
 
 // Function call to initialize app
 init();
